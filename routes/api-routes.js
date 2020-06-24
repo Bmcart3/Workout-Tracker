4dc30ebbf3a9010a560b3f03388c35f958373669
bmcart3 <bmcart3@gmail.com>
2020-06-24 18:27:07 -0400

    put route for adding an exercise works

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,16 +12,6 @@ module.exports = function (app) {
               });
     });
     
-    app.get("/api/exercise", (req, res) => {
-      db.Workout.find({})
-      .then(Workout => {
-          res.json(Workout);
-      })
-      .catch(err => {
-          res.json(err);
-        });
-    });
-    
     app.post("/api/workouts", (req,res) => {
       const workout = new Workout(req.body)
       db.Workout.create(workout)
@@ -32,4 +22,24 @@ module.exports = function (app) {
           res.json(err);
         });
     });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body}})
+            .then(workout => {
+                res.json(workout);
+            })
+            .catch(err => {
+                res.json(err)
+            });
+    });
+
+    app.get("/api/exercise", (req, res) => {
+        db.Workout.find({})
+        .then(Workout => {
+            res.json(Workout);
+        })
+        .catch(err => {
+            res.json(err);
+          });
+      });
 };
